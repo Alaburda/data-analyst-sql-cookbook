9f5fe3eed3a96c07fd61d8ed415c90f70da851a3
Alaburda <p.alaburda1@gmail.com>
2023-11-11 23:17:42 +0200

    adding pk to subscriptions

--- scripts/filtering.R
+++ scripts/filtering.R
@@ -7,12 +7,15 @@ users <- data.frame(id = 1:100000,
                     created_channel = sample(c(1:5), size = 100000, replace = TRUE),
                     created_at = seq.Date(from = as.Date("2023-01-01"), to = as.Date("2023-03-01"), length.out = 200))
 
-subscriptions <- data.frame(id = sample(users$id, 10000, replace = FALSE),
+subscriptions <- data.frame(id = 1:10000,
+                            user_id = sample(users$id, 10000, replace = FALSE),
                             subscription_type = sample(c(1:4), size = 10000, replace = TRUE))
 
 dbWriteTable(con, "subscribers", subscriptions, overwrite = TRUE)
 
 dbWriteTable(con, "users", users, overwrite = TRUE)
+
+dbDisconnect(con)
 
 tic()
 dbGetQuery(con, "explain query plan
